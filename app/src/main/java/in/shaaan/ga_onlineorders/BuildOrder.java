497634da58fc041218fdc387e45374a5dc27e641
shaaan <shaaan.xda@gmail.com>
2017-03-05 18:39:17 +0530

    Initial commit

---
+++ app/src/main/java/in/shaaan/ga_onlineorders/BuildOrder.java
@@ -0,0 +1,54 @@
+package in.shaaan.ga_onlineorders;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class BuildOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_order);
+
+        int layoutItemId = android.R.layout.simple_dropdown_item_1line;
+        String[] drugArr = getResources().getStringArray(R.array.drugList);
+        List<String> drugList = Arrays.asList(drugArr);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, layoutItemId, drugList);
+
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autocompleteview);
+        autoCompleteTextView.setAdapter(adapter);
+    }
+
+    public void addProduct(View view) {
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autocompleteview);
+        EditText editText = (EditText) findViewById(R.id.quantity);
+        String quantity = editText.getText().toString();
+        String drug = autoCompleteTextView.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.prodList);
+        if (drug.matches("")) {
+            Snackbar.make(view, "You did not enter the product", Snackbar.LENGTH_LONG).show();
+        }
+        else if (quantity.matches("")) {
+            Snackbar.make(view, "You did not add the quantity", Snackbar.LENGTH_LONG).show();
+        } else
+        textView.append("" + drug + "     " + quantity + "\n");
+        autoCompleteTextView.getText().clear();
+        editText.getText().clear();
+        autoCompleteTextView.requestFocus();
+    }
+
+    public void sendOrder(View view) {
+
+    }
+
+}
