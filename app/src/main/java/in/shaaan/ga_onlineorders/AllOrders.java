689e5680905808b8359637b2949135b06408d5dd
shaaan <shaaan.xda@gmail.com>
2017-03-20 14:14:32 +0530

    Add signout option, disable smartlock, update logic

--- app/src/main/java/in/shaaan/ga_onlineorders/AllOrders.java
+++ app/src/main/java/in/shaaan/ga_onlineorders/AllOrders.java
@@ -2,14 +2,19 @@ package in.shaaan.ga_onlineorders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -19,8 +24,16 @@ import butterknife.ButterKnife;
 public class AllOrders extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 7410;
+    private static final String TAG = "AnonymousAuth";
     @Bind(R.id.fab)
     FloatingActionButton floatingActionButton;
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
 
 
     @Override
@@ -32,33 +45,71 @@ public class AllOrders extends AppCompatActivity {
         setSupportActionBar(toolbar);
         floatingActionButton.setVisibility(View.INVISIBLE);
 
-        Thread thread = new Thread(new Runnable() {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void run() {
-                FirebaseAuth auth = FirebaseAuth.getInstance();
-                if (auth.getCurrentUser() != null) {
-                    // already signed in
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
                     Snackbar.make(findViewById(R.id.coordinatorLayout), "Logged in successfully..", Snackbar.LENGTH_LONG).show();
                     floatingActionButton.setVisibility(View.VISIBLE);
+                    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(AllOrders.this, BuildOrder.class);
+                            startActivity(intent);
+                        }
+                    });
                 } else {
-                    // not signed in
-                    startActivityForResult(
-                            // Get an instance of AuthUI based on the default app
-                            AuthUI.getInstance().createSignInIntentBuilder().setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())).build(),
-                            RC_SIGN_IN);
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    floatingActionButton.setVisibility(View.VISIBLE);
+                    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Thread thread = new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                                    if (auth.getCurrentUser() != null) {
+                                        // already signed in
+                                        Log.d(TAG, "already logged in");
+                                    } else {
+                                        // not signed in
+                                        startActivityForResult(
+                                                // Get an instance of AuthUI based on the default app
+                                                AuthUI.getInstance().createSignInIntentBuilder().setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+                                                        .setIsSmartLockEnabled(false)
+                                                        .build(),
+                                                RC_SIGN_IN);
+                                    }
+                                }
+                            });
+                            thread.start();
+                        }
+                    });
                 }
             }
-        });
-        thread.start();
+        };
+//        mAuth.addAuthStateListener(mAuthListener);
+
     }
 
-
-    public void buildOrder(View view) {
+    /*public void buildOrder(View view) {
         Intent intent = new Intent(this, BuildOrder.class);
         startActivity(intent);
+    }*/
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
-   /* @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_all_orders, menu);
@@ -74,9 +125,12 @@ public class AllOrders extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+//            return true;
+            AuthUI.getInstance()
+                    .signOut(this);
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
