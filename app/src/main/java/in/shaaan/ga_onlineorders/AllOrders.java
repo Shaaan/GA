5af0590c6fa75f7e95f1becf140ae97ece2e891c
shaaan <shantanulondhe26@gmail.com>
2017-03-28 22:13:31 +0530

    Show the user if he's logged in

--- app/src/main/java/in/shaaan/ga_onlineorders/AllOrders.java
+++ app/src/main/java/in/shaaan/ga_onlineorders/AllOrders.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -45,6 +46,7 @@ public class AllOrders extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Snackbar.make(findViewById(R.id.coordinatorLayout), "Logged in successfully", Snackbar.LENGTH_LONG);
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
