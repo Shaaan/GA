4f8e57217792d2a602d0f171f3ad6b7f9de17a42
Shaaan <shaaan.xda@gmail.com>
2017-10-11 14:09:26 +0530

    Add list divider

--- app/src/main/java/in/shaaan/ga_onlineorders/BuildOrder.java
+++ app/src/main/java/in/shaaan/ga_onlineorders/BuildOrder.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -84,6 +86,8 @@ public class BuildOrder extends AppCompatActivity {
 
         autoCompleteTextView.setAdapter(adapter);
         linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.addItemDecoration(new android.support.v7.widget.DividerItemDecoration(recyclerView.getContext(), android.support.v7.widget.DividerItemDecoration.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setLayoutManager(linearLayoutManager);
         mAdapter = new RecyclerAdapterFile(orderData);
         recyclerView.setAdapter(mAdapter);
@@ -147,9 +151,9 @@ public class BuildOrder extends AppCompatActivity {
         return instance;
     }
 
-//    public void sendOrder(View view) {
-//        submitOrder();
-//    }
+    public void sendOrder(View view) {
+        submitOrder();
+    }
 
     private void submitOrder() {
         final String customer = completeTextView.getText().toString();
